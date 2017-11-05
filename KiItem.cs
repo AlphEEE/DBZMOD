ad426b9f77cda658c48ed0a9e204636e5ad2a2d2
NuovaPrime <loganman51@hotmail.com>
2017-11-05 14:09:32 -0700

    added ki class

---
+++ KiItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using DBZMOD;
+
+namespace DBZMOD
+{
+    public abstract class KiItem : ModItem
+    {
+        // make-safe
+        public override void SetDefaults()
+        {
+            item.melee = false;
+            item.ranged = false;
+            item.magic = false;
+            item.thrown = false;
+            item.summon = false;
+        }
+
+        public override void GetWeaponKnockback(Player player, ref float knockback)
+        {
+            MyPlayer modPlayer = MyPlayer.GetModPlayer(player);
+            knockback += modPlayer.KiKbAddition;
+        }
+        public override void GetWeaponDamage(Player player, ref int damage)
+        {
+            MyPlayer modPlayer = MyPlayer.GetModPlayer(player);
+            // We want to multiply the damage we do by our alchemicalDamage modifier.
+            // todo: ? do we want magic damage to also have effect here?
+            damage = (int)(damage * modPlayer.KiDamage + 5E-06f);
+        }
+    }
+}
--- MyPlayer.cs
+++ MyPlayer.cs
@@ -14,14 +14,13 @@ namespace DBZMOD
     {
     	public float KiDamage;
         public float KiKbAddition;
-        public int KiCrit;
         public bool ZoneCustomBiome = false;
-        
+
         public override void ResetEffects()
         {
             KiDamage = 1f;
             KiKbAddition = 0f;
-            KiCrit = 0;
+        }
 	
         public override void SetupStartInventory(IList<Item> items)
         {
@@ -30,38 +29,38 @@ namespace DBZMOD
             item4.stack = 1;         
             items.Add(item4);
         }
-		            //public override void UpdateBiomes()
-           
-		   //ZoneCustomBiome = (DBZMODWorld.customBiome > 0);  
+        //public override void UpdateBiomes()
 
-       //public static readonly PlayerLayer MiscEffectsBack = new PlayerLayer("DBZMOD", "MiscEffectsBack", PlayerLayer.MiscEffectsBack, delegate(PlayerDrawInfo drawInfo)
-			//{
-				//if (drawInfo.shadow != 0f)
-				//{
-					//return;
-				//}
-				//Player drawPlayer = drawInfo.drawPlayer;
-				//Mod mod = ModLoader.GetMod("DBZMOD");
-				//MyPlayer modPlayer = drawPlayer.GetModPlayer<MyPlayer>(mod);
-				//if (player.FindBuffIndex(mod.BuffType("KaiokenBuff")) != -1)
-				//{
-					//Texture2D texture = mod.GetTexture("Auras/KaiokenAura");
-					//int drawX = (int)(drawInfo.position.X + drawPlayer.width / 2f);
-					//int drawY = (int)(drawInfo.position.Y + drawPlayer.height / 2f);
-					//DrawData value6 = new DrawData(TextureManager.Load("Auras/KaiokenAura"), value4 + new Vector2(75f, 75f), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, 0, 150, 150)), new Color(new Vector4(1f - (float)Math.Sqrt((double)num91))), drawPlayer.bodyRotation, new Vector2(75f, 75f), (1f + num91), SpriteEffects.None, 0);
-					//Main.playerDrawData.Add(data);
-				//}
-			//});
-		
-		//public override void ModifyDrawLayers(List<PlayerLayer> layers)
-		//{
-			//MiscEffectsBack.visible = true;
-			//layers.Insert(0, MiscEffectsBack);
-		//}
+        //ZoneCustomBiome = (DBZMODWorld.customBiome > 0);  
 
+        //public static readonly PlayerLayer MiscEffectsBack = new PlayerLayer("DBZMOD", "MiscEffectsBack", PlayerLayer.MiscEffectsBack, delegate(PlayerDrawInfo drawInfo)
+        //{
+        //if (drawInfo.shadow != 0f)
+        //{
+        //return;
+        //}
+        //Player drawPlayer = drawInfo.drawPlayer;
+        //Mod mod = ModLoader.GetMod("DBZMOD");
+        //MyPlayer modPlayer = drawPlayer.GetModPlayer<MyPlayer>(mod);
+        //if (player.FindBuffIndex(mod.BuffType("KaiokenBuff")) != -1)
+        //{
+        //Texture2D texture = mod.GetTexture("Auras/KaiokenAura");
+        //int drawX = (int)(drawInfo.position.X + drawPlayer.width / 2f);
+        //int drawY = (int)(drawInfo.position.Y + drawPlayer.height / 2f);
+        //DrawData value6 = new DrawData(TextureManager.Load("Auras/KaiokenAura"), value4 + new Vector2(75f, 75f), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, 0, 150, 150)), new Color(new Vector4(1f - (float)Math.Sqrt((double)num91))), drawPlayer.bodyRotation, new Vector2(75f, 75f), (1f + num91), SpriteEffects.None, 0);
+        //Main.playerDrawData.Add(data);
+        //}
+        //});
 
+        //public override void ModifyDrawLayers(List<PlayerLayer> layers)
+        //{
+        //MiscEffectsBack.visible = true;
+        //layers.Insert(0, MiscEffectsBack);
+        //}
 
 
-		   
+
+
+
     }
 }
