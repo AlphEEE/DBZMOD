7723a2a43c81968417c59df8a10d36149ed0e963
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-07 14:52:30 -0500

    More Spread

--- Items/Weapons/KiBlast.cs
+++ Items/Weapons/KiBlast.cs
@@ -31,7 +31,7 @@ namespace DBZMOD.Items.Weapons
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
-			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15));
 			speedX = perturbedSpeed.X;
 			speedY = perturbedSpeed.Y;
 			return true;
