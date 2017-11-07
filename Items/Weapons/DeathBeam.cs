5aa781dd23197bea213af6e6eaf4ad814adaad0e
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-07 15:08:59 -0500

    Revert "Deathbeam"
    This reverts commit d623152d6db43e12eca29b9156270f65b3328756.

--- Items/Weapons/DeathBeam.cs
+++ Items/Weapons/DeathBeam.cs
@@ -1,52 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Graphics;
+using System;
 using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace DBZMOD.Items.Weapons
 {
-	public class DeathBeam: KiItem
+	public class DeathBeam : ModItem
 	{
 		public override void SetDefaults()
 		{
-			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
-			item.shoot = mod.ProjectileType("DeathBeamProjectile");
-			item.shootSpeed = 70f;
-			item.damage = 35;
-			item.knockBack = 5f;
-			item.useStyle = 5;
-			item.UseSound = SoundID.Item12;
-			item.useAnimation = 25;
-			item.useTime = 14;
-			item.width = 40;
-			item.noUseGraphic = true;
-			item.height = 40;
-			item.autoReuse = true;
-			item.value = Item.sellPrice(0, 0, 5, 0);
-			item.rare = 1;
-	    }
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 55f;
-			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
-			{
-				position += muzzleOffset;
-			}
-			return true;
+			item.magic = true;
+			item.damage = 40;
+
+			item.width = 28;
+			item.height = 32;
+			item.useTime = 30;
+			item.useAnimation = 30;
+			item.useStyle = 5;
+			item.noMelee = true;
+			item.knockBack = 8;
+			item.value = 500000;
+			item.rare = 3;
+			item.mana = 25;
+			item.UseSound = SoundID.Item21;
+			item.autoReuse = false;
+			item.shoot = mod.ProjectileType ("DeathBeamProjectile");
+			item.shootSpeed = 8f;
 		}
-		
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-	        recipe.AddIngredient(3);
-            recipe.AddTile(TileID.LunarCraftingStation);
-			recipe.SetResult(this);
-	        recipe.AddRecipe();
-		}
+
+    public override void SetStaticDefaults()
+    {
+      DisplayName.SetDefault("Death Beam");
+      Tooltip.SetDefault("Takes About 10 Episodes Before It Hits Something.");
+    }
+
 	}
 }
--- Projectiles/DeathBeamProjectile.cs
+++ Projectiles/DeathBeamProjectile.cs
@@ -1,58 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Graphics;
+using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace DBZMOD.Projectiles
 {
-    public class DeathBeamProjectile : KiProjectile
-    {
-    	public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("DeathBeamProjectile");
-		}
-    	
-        public override void SetDefaults()
+	public class DeathBeamProjectile : ModProjectile
+	{
+        public override void SetStaticDefaults()
         {
-            projectile.width = 24;
-            projectile.height = 204;
-			projectile.aiStyle = 1;
-			projectile.light = 1f;
-			projectile.alpha = 220;
-            projectile.friendly = true;
-            projectile.ignoreWater = true;
-            projectile.penetrate = 2;
-            projectile.timeLeft = 120;
-			projectile.tileCollide = false;
-			ProjectileID.Sets.TrailCacheLength[projectile.type] = 12;
-            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            DisplayName.SetDefault("Death Beam");
         }
-   
-        public override void Kill(int timeLeft)
-        {
-            for (int num303 = 0; num303 < 3; num303++)
-			{
-				int num304 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 20, 0f, 0f, 100, default(Color), 0.8f);
-				Main.dust[num304].noGravity = true;
-				Main.dust[num304].velocity *= 1.2f;
-				Main.dust[num304].velocity -= projectile.oldVelocity * 0.3f;
-			}
-		}
-		
-		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        public override void SetDefaults()
 		{
-			Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
-			for (int k = 0; k < projectile.oldPos.Length; k++)
-			{
-				Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
-				Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
-				spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+			projectile.width = 12;
+			projectile.height = 102;
+            projectile.friendly = true;
+            projectile.penetrate = -1;                       
+            Main.projFrames[projectile.type] = 1;           
+            projectile.hostile = false;
+			projectile.magic = true;                        
+            projectile.tileCollide = false;                 
+			projectile.ignoreWater = true;
+		}
+		public override void AI()
+		{   
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+            projectile.localAI[0] += 1f;
+			projectile.alpha = (int)projectile.localAI[0] * 2;
+			
+			if (projectile.localAI[0] > 130f)
+            {
+				projectile.Kill();
 			}
-			return true;	
-		}   
-    }
+           
+        }
+	}
 }
