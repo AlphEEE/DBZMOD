044584709c1cc4f2c57e155e57fe952f39eef988
NuovaPrime <loganman51@hotmail.com>
2017-11-11 13:31:02 -0700

    uhg

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -57,27 +57,27 @@ namespace DBZMOD
 
         //ZoneCustomBiome = (DBZMODWorld.customBiome > 0);  
 
-        private static readonly PlayerLayer miscEffectsBack = new PlayerLayer("DBZMOD", "MiscEffectsBack", PlayerLayer.MiscEffectsBack, delegate (PlayerDrawInfo drawInfo)
-             {
-                 if (drawInfo.shadow != 0f)
-                 {
-                     return;
-                 }
-                 Player drawPlayer = drawInfo.drawPlayer;
-                 Mod mod = ModLoader.GetMod("DBZMOD");
-                 MyPlayer modPlayer = drawPlayer.GetModPlayer<MyPlayer>(mod);
-                 if (player.FindBuffIndex(mod.BuffType("KaiokenBuff")) != -1)
-                 {
-                     Texture2D texture = mod.GetTexture("Auras/KaiokenAura");
-                     int drawX = (int)(drawInfo.position.X + drawPlayer.width / 2f);
-                     int drawY = (int)(drawInfo.position.Y + drawPlayer.height / 2f);
-                     DrawData value6 = new DrawData(TextureManager.Load("Auras/KaiokenAura"), value4 + new Vector2(75f, 75f), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, 0, 150, 150)), new Color(new Vector4(1f - (float)Math.Sqrt((double)num91))), drawPlayer.bodyRotation, new Vector2(75f, 75f), (1f + num91), SpriteEffects.None, 0);
-                     Main.playerDrawData.Add(DrawData);
-                 }
-             });
-        private static double num91;
-        private static Vector2 value4;
-        private PlayerLayer MiscEffectsBack;
+        //private static readonly PlayerLayer miscEffectsBack = new PlayerLayer("DBZMOD", "MiscEffectsBack", PlayerLayer.MiscEffectsBack, delegate (PlayerDrawInfo drawInfo)
+            // {
+               //  if (drawInfo.shadow != 0f)
+                // {
+                 //    return;
+                // }
+                 //Player drawPlayer = drawInfo.drawPlayer;
+                // Mod mod = ModLoader.GetMod("DBZMOD");
+               //  MyPlayer modPlayer = drawPlayer.GetModPlayer<MyPlayer>(mod);
+               //  if (player.FindBuffIndex(mod.BuffType("KaiokenBuff")) != -1)
+                // {
+                //     Texture2D texture = mod.GetTexture("Auras/KaiokenAura");
+                //     int drawX = (int)(drawInfo.position.X + drawPlayer.width / 2f);
+                //     int drawY = (int)(drawInfo.position.Y + drawPlayer.height / 2f);
+                //     DrawData value6 = new DrawData(TextureManager.Load("Auras/KaiokenAura"), value4 + new Vector2(75f, 75f), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, 0, 150, 150)), new Color(new Vector4(1f - (float)Math.Sqrt((double)num91))), drawPlayer.bodyRotation, new Vector2(75f, 75f), (1f + num91), SpriteEffects.None, 0);
+                //     Main.playerDrawData.Add(DrawData);
+               //  }
+            // });
+      //  private static double num91;
+      //  private static Vector2 value4;
+      //  private PlayerLayer MiscEffectsBack;
 
         public override void PostUpdateEquips()
         {
@@ -90,10 +90,10 @@ namespace DBZMOD
             MyPlayer.ModPlayer(player).FortitudeStat += 1;
         }
         
-        public override void ModifyDrawLayers(List<PlayerLayer> layers)
-            {
-                MiscEffectsBack.visible = true;
-                layers.Insert(0, MiscEffectsBack);
-            }
+      //  public override void ModifyDrawLayers(List<PlayerLayer> layers)
+          //  {
+          //      MiscEffectsBack.visible = true;
+          //      layers.Insert(0, MiscEffectsBack);
+          //  }
     }
 }
