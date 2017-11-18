df57b6ea3451628a467ba28a4f4e7441cc58d200
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-18 09:24:41 -0500

    Revert "Hopefully fixed it"
    This reverts commit 5e7f51313cbfee4104411ae90e77f8623c735ef3.

--- MyPlayer.cs
+++ MyPlayer.cs
@@ -9,11 +9,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Graphics;
 using Terraria.GameInput;
-using Terraria.Localization;
-using System.IO;
 using DBZMOD;
-using Terraria.UI;
-using Terraria.DataStructures;
 
 namespace DBZMOD
 {
