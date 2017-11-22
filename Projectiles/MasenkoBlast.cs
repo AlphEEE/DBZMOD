9852ba662aadbdef79efc04e7ed176b027e991ee
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-22 16:04:29 -0500

    a
    a

--- Projectiles/MasenkoBlast.cs
+++ Projectiles/MasenkoBlast.cs
@@ -52,7 +52,7 @@ namespace DBZMOD.Projectiles
 			 projectile.ai[1]++;
             if(projectile.ai[1] == 3)
             {
-                Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y - 0, 0, 50, mod.ProjectileType("MasenkoTrail"), projectile.damage, 4f, projectile.owner, 0, projectile.rotation);
+                Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y - 0, 0, 0, mod.ProjectileType("MasenkoTrail"), projectile.damage, 4f, projectile.owner, 0, projectile.rotation);
                 projectile.ai[1] = 0;
             }
         }
