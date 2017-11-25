c9f615b88a0d47ccc95045cf9620e930e2295752
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-25 10:55:16 -0500

    extras
    a

--- Items/Weapons/EnergyWaveVolley.cs
+++ Items/Weapons/EnergyWaveVolley.cs
@@ -16,7 +16,7 @@ namespace DBZMOD.Items.Weapons
 			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
 			item.shoot = mod.ProjectileType("EnergyWaveVolleyBlast");
 			item.shootSpeed = 90f;
-			item.damage = 28;
+			item.damage = 45;
 			item.knockBack = 2f;
 			item.useStyle = 5;
 			item.UseSound = SoundID.Item12;
