9a32cb92a3b58f8729b74b04ce1379da9e557f5b
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-11-25 20:13:37 -0500

    A
    WIP SPIRIT BOMB

---
+++ Items/Weapons/SpiritBomb.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Items.Weapons
+{
+	public class SpiritBomb : KiItem
+	{
+		public override void SetDefaults()
+		{
+			item.shoot = mod.ProjectileType("SpiritBombProjectile");
+			item.shootSpeed = 10f;
+			item.damage = 300;
+			item.knockBack = 12f;
+			item.useStyle = 1;
+			item.UseSound = SoundID.Item1;
+			item.useAnimation = 25;
+			item.useTime = 180;
+			item.width = 40;
+			item.channel = true;
+			item.noUseGraphic = true;
+			item.height = 40;
+			item.autoReuse = false;
+			item.value = Item.sellPrice(0, 0, 5, 0);
+			item.rare = 4;
+	    }
+		
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 1 + Main.rand.Next(1);  //This defines how many projectiles to shot
+            for (int index = 0; index < numberProjectiles; ++index)
+            {
+                Vector2 vector2_1 = new Vector2((float)((double)player.position.X + (double)player.width * 0.5 + (double)(Main.rand.Next(201) * -player.direction) + ((double)Main.mouseX + (double)Main.screenPosition.X - (double)player.position.X)), (float)((double)player.position.Y + (double)player.height * 0.5 - 600.0));   //this defines the projectile width, direction and position
+                vector2_1.X = (float)(((double)vector2_1.X + (double)player.Center.X) / 2.0) + (float)Main.rand.Next(-200, 201);
+                vector2_1.Y -= (float)(100 * index);
+                float num12 = (float)Main.mouseX + Main.screenPosition.X - vector2_1.X;
+                float num13 = (float)Main.mouseY + Main.screenPosition.Y - vector2_1.Y;
+                if ((double)num13 < 0.0) num13 *= -1f;
+                if ((double)num13 < 20.0) num13 = 20f;
+                float num14 = (float)Math.Sqrt((double)num12 * (double)num12 + (double)num13 * (double)num13);
+                float num15 = item.shootSpeed / num14;
+                float num16 = num12 * num15;
+                float num17 = num13 * num15;
+                float SpeedX = num16 + (float)Main.rand.Next(-40, 41) * 0.02f;  //this defines the projectile X position speed and randomnes
+                float SpeedY = num17 + (float)Main.rand.Next(-40, 41) * 0.02f;  //this defines the projectile Y position speed and randomnes
+                Projectile.NewProjectile(vector2_1.X, vector2_1.Y, SpeedX, SpeedY, type, damage, knockBack, Main.myPlayer, 0.0f, (float)Main.rand.Next(5));
+            }
+            return false;
+        }
+		
+	    public override void SetStaticDefaults()
+		{
+		Tooltip.SetDefault("-Tier 4-");
+		DisplayName.SetDefault("Spirit Bomb");
+		}
+        public override bool UseItem(Player player)
+        {
+            MyPlayer.ModPlayer(player).KiControlStat += 1;
+            return true;
+        }
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "PridefulKiCrystal", 50);
+		    recipe.AddIngredient(null, "AngerKiCrystal", 50);
+            recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+	        recipe.AddRecipe();
+		}
+	}
+}
---
+++ Projectiles/SpiritBombProjectile.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Projectiles
+{
+    public class SpiritBombProjectile : KiProjectile
+    {
+        public override Color? GetAlpha(Color lightColor)
+        {
+    return Color.White;
+        }
+		
+    	public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Spirit Bomb Projectile");
+		}
+    	
+        public override void SetDefaults()
+        {
+            projectile.width = 400;
+            projectile.height = 400;
+			projectile.aiStyle = 1;
+			projectile.light = 0.8f;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 400;
+			aiType = 14;
+			projectile.aiStyle = 1;
+			projectile.tileCollide = false;
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 12;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+		public override void AI()
+        {
+			if (Vector2.Distance(projectile.position, Main.MouseWorld) > 0.1)
+            projectile.velocity = Vector2.Normalize(Main.MouseWorld - projectile.position) * 13;
+        	projectile.ai[0] += 1f;
+			if (projectile.ai[0] >= 240f)
+			{
+				projectile.alpha += 3;
+				projectile.damage = (int)((double)projectile.damage * 0.95);
+				projectile.knockBack = (float)((int)((double)projectile.knockBack * 0.95));
+			}
+			if (projectile.ai[0] < 240f)
+			{
+				projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+			}
+			if (projectile.velocity.Y > 16f)
+			{
+				projectile.velocity.Y = 16f;
+			}
+        	float num472 = projectile.Center.X;
+			float num473 = projectile.Center.Y;
+			float num474 = 800f;
+			bool flag17 = false;
+			for (int num475 = 0; num475 < 200; num475++)
+			{
+				if (Main.npc[num475].CanBeChasedBy(projectile, false) && Collision.CanHit(projectile.Center, 1, 1, Main.npc[num475].Center, 1, 1))
+				{
+					float num476 = Main.npc[num475].position.X + (float)(Main.npc[num475].width / 2);
+					float num477 = Main.npc[num475].position.Y + (float)(Main.npc[num475].height / 2);
+					float num478 = Math.Abs(projectile.position.X + (float)(projectile.width / 2) - num476) + Math.Abs(projectile.position.Y + (float)(projectile.height / 2) - num477);
+					if (num478 < num474)
+					{
+						num474 = num478;
+						num472 = num476;
+						num473 = num477;
+						flag17 = true;
+					}
+				}
+			}
+			if (flag17)
+			{
+				float num483 = 18f;
+				Vector2 vector35 = new Vector2(projectile.position.X + (float)projectile.width * 0.5f, projectile.position.Y + (float)projectile.height * 0.5f);
+				float num484 = num472 - vector35.X;
+				float num485 = num473 - vector35.Y;
+				float num486 = (float)Math.Sqrt((double)(num484 * num484 + num485 * num485));
+				num486 = num483 / num486;
+				num484 *= num486;
+				num485 *= num486;
+				projectile.velocity.X = (projectile.velocity.X * 20f + num484) / 21f;
+				projectile.velocity.Y = (projectile.velocity.Y * 20f + num485) / 21f;
+				return;
+			}
+            if (Main.rand.Next(6) == 0)
+            {
+		}
+	}
+			
+        public override void Kill(int timeLeft)
+        {
+            for (int num303 = 0; num303 < 3; num303++)
+			{
+				int num304 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 20, 0f, 0f, 100, default(Color), 0.8f);
+				Main.dust[num304].noGravity = true;
+				Main.dust[num304].velocity *= 1.2f;
+				Main.dust[num304].velocity -= projectile.oldVelocity * 0.3f;
+			}
+        }
+		
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+			for (int k = 0; k < projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+				Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+				spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+			}
+			return true;	
+		}   
+    }
+}
