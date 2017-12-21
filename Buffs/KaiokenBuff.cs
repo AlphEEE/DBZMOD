a7a580aa49695bfc476cc773b70b318beff8ac42
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2017-12-21 14:23:44 -0500

    ah
    ah

--- Buffs/KaiokenBuff.cs
+++ Buffs/KaiokenBuff.cs
@@ -14,14 +14,15 @@ namespace DBZMOD.Buffs
         public override void Update(Player player, ref int buffIndex)
         {                                             
             player.lifeRegen = -20;
-			player.moveSpeed += 3f;
-            player.meleeDamage += 2f;
-			player.rangedDamage += 2f;
-			player.magicDamage += 2f;
-			player.minionDamage += 2f;
-			player.thrownDamage += 2f;
+			player.moveSpeed *= 3f;
+            player.meleeDamage *= 2f;
+			player.rangedDamage *= 2f;
+			player.magicDamage *= 2f;
+			player.minionDamage *= 2f;
+			player.thrownDamage *= 2f;
 			MyPlayer.ModPlayer(player).KiDamage *= 2f;
-			Lighting.AddLight((int)(p.Center.X / 16), (int)(p.Center.Y / 16), 10, 0, 0);
+			player.buffImmune[BuffID.Shine] = true;
+			Lighting.AddLight(player.Center, 1.1f, 1.15f, 1.2f);
         }	
 	}
 }
